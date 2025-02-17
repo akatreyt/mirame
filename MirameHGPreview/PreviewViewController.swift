6dcd16eb19132ab6c890cde18eff2526d03f176f
Trey t <treytartt@fastmail.com>
2025-02-17 12:20:07 -0600

    fix upload error

--- MirameHGPreview/PreviewViewController.swift
+++ MirameHGPreview/PreviewViewController.swift
@@ -11,26 +11,16 @@ import QuickLook
 
 class PreviewViewController: UIViewController, QLPreviewingController {
 
-    override func viewDidLoad() {
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad()  {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-    /*
-    func preparePreviewOfSearchableItem(identifier: String, queryString: String?) async throws {
-        // Implement this method and set QLSupportsSearchableItems to YES in the Info.plist of the extension if you support CoreSpotlight.
-
-        // Perform any setup necessary in order to prepare the view.
-        // Quick Look will display a loading spinner until this returns.
-    }
-    */
-
-    func preparePreviewOfFile(at url: URL) async throws {
-        // Add the supported content types to the QLSupportedContentTypes array in the Info.plist of the extension.
-
-        // Perform any setup necessary in order to prepare the view.
-
-        // Quick Look will display a loading spinner until this returns.
+    
+    func preparePreviewOfFile(at url: URL, completionHandler handler: @escaping (Error?) -> Void)  {
+        label.text = "Tap share below to open in mírame"
+        handler(nil)
     }
 
 }
